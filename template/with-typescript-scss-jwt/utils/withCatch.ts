6a442445c450662cd95b814307e31aa12ada26f1
임재성 <personalc6@github.com>
2021-12-07 12:18:06 +0900

    update

--- template/with-typescript-scss-jwt/utils/withCatch.ts
+++ template/with-typescript-scss-jwt/utils/withCatch.ts
@@ -4,6 +4,6 @@ export const withCatch = (handler: (req: NextApiRequest, res: NextApiResponse) =
     try {
         await handler(req, res)
     } catch (err: any) {
-        res.status(err.code).json(err)
+        res.status(err.code || 500).json(err)
     }
 }
